ebe39d0d60a4e27ebf8f3b78a59e6a454c6a142b
Bailey Danyluk <baileydanyluk@gmail.com>
2025-09-06 21:13:55 -0600

    fix headers

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -1,4 +1,4 @@
-#include <engine/engine.hpp>
+#include <engine/engine.h>
 
 namespace Engine {
     Status status() {
--- game/main.cpp
+++ game/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <engine/engine.hpp>
+#include <engine/engine.h>
 
 int main() {
     std::cout << "Hello, world!" << std::endl;
