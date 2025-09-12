8c856f0c2f2fa1395fb5d83eec7e3799e11dc6b4
Bailey Danyluk <baileydanyluk@gmail.com>
2025-09-12 15:11:24 -0600

    split benchmarks and tests

---
+++ tests/linalg/bench_vector2_float.cpp
@@ -0,0 +1,103 @@
+#include <vector>
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/generators/catch_generators_adapters.hpp>
+#include <catch2/generators/catch_generators_random.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+#include <engine/random.h>
+
+#include <linalg/vector.h>
+#include <linalg/vector_ops.h>
+
+#define DESIRED_PRECISION (1e-10f)
+
+constexpr auto range() -> Range<float> {
+    return Range<float>{ -250000.f, 250000.f };
+}
+
+TEST_CASE( "blas1 - bench [Vector2<float>]", "[blas1][Vector2][bench]" ) {
+    BENCHMARK_ADVANCED("blas1::axpy - random")(Catch::Benchmark::Chronometer meter) {
+        auto rng = Random();
+        auto a = rng.range<float>(range());
+        auto x = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        auto y = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        meter.measure([&] { return linalg::blas1::axpy(a, x, y); });
+    };
+    BENCHMARK_ADVANCED("blas1::scale - random")(Catch::Benchmark::Chronometer meter) {
+        auto rng = Random();
+        auto a = rng.range<float>(range());
+        auto x = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        meter.measure([&] { return linalg::blas1::scale(a, x); });
+    };
+    BENCHMARK_ADVANCED("blas1::copy - random")(Catch::Benchmark::Chronometer meter) {
+        auto rng = Random();
+        auto x = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        auto y = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        meter.measure([&] { linalg::blas1::copy(x, y); return x; });
+    };
+    BENCHMARK_ADVANCED("blas1::swap - random")(Catch::Benchmark::Chronometer meter) {
+        auto rng = Random();
+        auto x = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        auto y = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        meter.measure([&] { linalg::blas1::copy(x, y); return x; });
+    };
+    BENCHMARK_ADVANCED("blas1::dot - random")(Catch::Benchmark::Chronometer meter) {
+        auto rng = Random();
+        auto x = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        auto y = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        meter.measure([&] { linalg::blas1::copy(x, y); return x; });
+    };
+    BENCHMARK_ADVANCED("blas1::component_sum - random")(Catch::Benchmark::Chronometer meter) {
+        auto rng = Random();
+        auto x = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        meter.measure([&] { return linalg::blas1::component_sum(x); });
+    };
+    BENCHMARK_ADVANCED("blas1::magnitude - random")(Catch::Benchmark::Chronometer meter) {
+        auto rng = Random();
+        auto x = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        meter.measure([&] { return linalg::blas1::magnitude(x); });
+    };
+    BENCHMARK_ADVANCED("blas1::component_max - random")(Catch::Benchmark::Chronometer meter) {
+        auto rng = Random();
+        auto x = linalg::Vector2<float>{
+            rng.range(range()),
+            rng.range(range())
+        };
+        meter.measure([&] { return linalg::blas1::component_max(x); });
+    };
+}
